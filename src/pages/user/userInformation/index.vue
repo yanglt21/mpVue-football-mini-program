31f280674a8a2d1fdb9d7c57fa40e96430f4f32b
马加奥 <majiaao@wecash.net>
2019-07-15 23:41:29 +0800

    完善个人信息页

--- src/pages/user/userInformation/index.vue
+++ src/pages/user/userInformation/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container flex-column-x-center">
     <div class="user-info-cell">
-        <div class="user-image">
-        </div>
+      <div class="user-image">
+        <image class="user-image-icon" :src="userIcon" mode="widthFix"></image>
+      </div>
+    </div>
+    <div class="width-90 flex-row-space-between user-info-cell" v-for="(item,index) in userInfoList" 
+         v-bind:key="index">
+         <div>{{item.title}}</div>
+         <div>{{item.value}}</div>
     </div>
     <div class="width-90 log-out" @click="logout">退出登录</div>
   </div>
@@ -18,44 +24,76 @@ export default {
   props: {},
   data () {
     return {
-      isLogin: false
+      isLogin: false,
+      userIcon: '',
+      userInfoList: []
     }
   },
   watch: {},
   computed: {},
   methods: {
     logout () {
-      this.$http.post(Url.logout, {
-        user_session: model.token || null
-      }).then((res) => {
-        const status = res.data.status
-        if (status) {
-          localStorage.clearUserInfo(model)
-          wx.switchTab({
-            url: '/pages/user/userCenter/main'
-          })
-        } else {
-          wx.showToast({
-            title: res.data.msg,
-            icon: 'error',
-            duration: 1500,
-            mask: false
-          })
-        }
-      })
-    },
-    fetchUserInfo() {
-        this.$http.post(Url.getUserInformation,{
-          user_id: model.userId
-        }).then((res)=> {
-          debugger
+      this.$http
+        .post(Url.logout, {
+          user_session: model.token || null
         })
+        .then(res => {
+          const status = res.data.status
+          if (status) {
+            localStorage.clearUserInfo(model)
+            wx.switchTab({
+              url: '/pages/user/userCenter/main'
+            })
+          } else {
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'error',
+              duration: 1500,
+              mask: false
+            })
+          }
+        })
+    },
+    fetchUserInfo () {
+      const self = this
+      this.$http
+        .post(Url.getUserInformation, {
+          open_id: model.openId
+        })
+        .then(res => {
+          if (res.data.status) {
+            const data = res.data.data
+            this.userIcon = data.avatar_url
+            self.standardUserInfo(data.user_info)
+          } else {
+            wx.showToast({
+              title: res.data.msg
+            })
+          }
+        })
+    },
+    standardUserInfo (userInfo) {
+      this.userInfoList.push({
+        title: '昵称',
+        value: userInfo.user_name
+      },
+      {
+        title: '性别',
+        value: userInfo.sex
+      }, {
+        title: '城市',
+        value: userInfo.province
+      }, {
+        title: '国籍',
+        value: userInfo.country
+      })
     }
   },
-  created () {
+  created () {},
+  mounted () {},
+  onShow () {
     this.fetchUserInfo()
-  },
-  mounted () {}
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -68,5 +106,21 @@ export default {
   line-height: 90rpx;
   text-align: center;
   border-radius: 10rpx;
+  margin-top: 100rpx;
+}
+.user-image-icon {
+  width: 100px;
+  border-radius: 50%;
+  border: 1px solid white;
+}
+.user-info-cell:nth-of-type(1) {
+  border: none;
+}
+.user-info-cell {
+  padding: 40rpx 0px;
+  color: white;
+  font-weight: 500;
+  font-size: 32rpx;
+  border-bottom: 1px solid #6441a5;
 }
 </style>
