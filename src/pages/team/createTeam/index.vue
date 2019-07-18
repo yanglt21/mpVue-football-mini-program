495bc228214fd11d928de3298a466f47545e9d2f
马加奥 <majiaao@wecash.net>
2019-07-18 17:07:50 +0800

    支持用户选择城市

--- src/pages/team/createTeam/index.vue
+++ src/pages/team/createTeam/index.vue
@@ -1,43 +1,85 @@
 <template>
-  <div class="container">
-    <van-notice-bar
-      text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。"
-    ></van-notice-bar>
-    <div class="width-90 information-cell">
+  <div class="container flex-column-x-center">
+    <div class="width-90 flex-row-space-between information-cell">
       <div class="information-cell-title">球队名</div>
-
+      <input type="text" class="information-cell-input" v-model="teamName">
     </div>
-    <div class="width-90 information-cell">
+    <div class="width-90 flex-row-space-between information-cell">
       <div class="information-cell-title">活动地区</div>
+      <div @click="handleShowPopup">选择</div>
+      <van-popup :show="showPopup" position="bottom" :close-on-click-overlay="true" v-on:close="onClose">
+        <van-area :area-list="areaList" 
+                  columns-num="2" title="请选择您球队所在的城市" 
+                  cancel-button-text="返回" @cancel="cancelSelectCity" 
+                  @confirm="confirmSelectCity" @change="handleChange">
+        </van-area>
+      </van-popup>
     </div>
-    <div class="width-90 information-cell">
+    <div class="width-90 flex-row-space-between information-cell">
       <div class="information-cell-title">主场颜色</div>
     </div>
-    <div class="width-90 information-cell">
+    <div class="width-90 flex-row-space-between information-cell">
       <div class="information-cell-title">介绍</div>
     </div>
   </div>
 </template>
 
 <script>
+const Area = require('../../../utils/area.js')
+let teamLocationTemp = {}
 export default {
   name: 'CreateTeam',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      teamName: '',
+      showPopup: false,
+      areaList: [],
+      teamLocation: {
+        province: '北京市',
+        city: '北京市'
+      }
+    }
   },
   watch: {},
   computed: {},
-  methods: {},
-  created () {},
-  mounted () {}
+  methods: {
+    handleShowPopup () {
+      this.showPopup = !this.showPopup
+    },
+    cancelSelectCity (ev) {
+      this.handleShowPopup()
+    },
+    confirmSelectCity (ev) {
+      this.teamLocation = teamLocationTemp
+      this.handleShowPopup()
+    },
+    handleChange () {
+      const obj = arguments[0].target.values
+      teamLocationTemp = {
+        province: obj[0].name,
+        city: obj[1].name
+      }
+    }
+  },
+  created () {
+  },
+  mounted () {
+    this.areaList = Area
+  }
 }
 </script>
 <style lang="scss" scoped>
 .information-cell {
-    &-title {
-        color: white;
-    }
+  color: white;
+  &-title {
+    color: white;
+  }
+  &-input {
+    background-color: white;
+    color: white;
+    border: 1px solid white;
+  }
 }
 </style>
